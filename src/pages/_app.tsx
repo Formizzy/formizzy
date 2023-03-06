069f7df288df642afe7beba509a9897896e9a1f4
Kartik Jetani <kartikjetani15@gmail.com>
2023-03-06 21:00:16 +0530

    + login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import AdminLayout from '@/layouts/AdminLayout'
 import '@/styles/globals.css'
+import { NextUIProvider } from '@nextui-org/react'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import themeConfig from "theme.config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <AdminLayout>
-    <Component {...pageProps} />
-  </AdminLayout>
+  const router = useRouter()
+  // if current url path includes /dashboard then use Admin layout as parent comp. 
+  // or else use page component only.
+  const isDashboardRoute = router.pathname.includes("/dashboard")
+
+  return <NextUIProvider theme={themeConfig}>
+    {isDashboardRoute ? <AdminLayout>
+      <Component {...pageProps} />
+    </AdminLayout> :
+      <Component {...pageProps} />}
+  </NextUIProvider>
 }
---
+++ theme.config.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@nextui-org/react";
+
+export default createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      primary: '#605BFF',
+      secondary: '#F9CB80',
+      error: '#FCC5D8',
+      btnBg: "#F7F7F8"
+    },
+  }
+})
